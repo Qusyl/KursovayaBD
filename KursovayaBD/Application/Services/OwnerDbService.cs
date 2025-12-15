93e8e0205235bd5aa00ce8249fa9e4775aa54142
Qusyl <95533550+Qusyl@users.noreply.github.com>
2025-12-15 19:19:26 +0300

    lab_15_12_2025V2

--- KursovayaBD/Application/Services/OwnerDbService.cs
+++ KursovayaBD/Application/Services/OwnerDbService.cs
@@ -1,4 +1,6 @@
 ﻿using KursovayaBD.Application.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace KursovayaBD.Application.Services
 {
@@ -11,6 +13,22 @@ namespace KursovayaBD.Application.Services
             _context = context;
         }
 
-        public async Task InsertOwner()
+        public async Task<string> InsertOwner(int Id, string Name, string Surname, string Lastname, string Address, string Phone)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync($"SELECT insert_owner({Id}, {Name}, {Surname}, {Lastname}, {Address}, {Phone})");
+                return "Запись успешно добавлена!";
+            }
+            catch (PostgresException E) when (E.SqlState == "23505") {
+                return "Запись с таким ID уже существует! ";
+            }
+            catch (Exception ex)
+            {
+                return $"Ошибка транзакции: {ex.Message}";
+            }
+
+        }
+        
     }
 }
