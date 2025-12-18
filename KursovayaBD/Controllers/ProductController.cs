f832b62c7c8f21970178944c0b736dd218d9e297
Qusyl <95533550+Qusyl@users.noreply.github.com>
2025-12-18 22:42:01 +0300

    Kursovaya_18_12_2025

---
+++ KursovayaBD/Controllers/ProductController.cs
@@ -0,0 +1,155 @@
+﻿using KursovayaBD.Models;
+using KursovayaBD.Repository.RepositoryImpl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KursovayaBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly RepositoryProduct repository;
+
+        public ProductController(RepositoryProduct repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<ProductModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            try
+            {
+                var products = await repository.GetAllAsync();
+
+                return Ok(products);
+            }
+
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Неизвестная ошибка: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ProductModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            try
+            {
+                var product = await repository.GetByIdAsync(id);
+                if (product == null)
+                {
+                    return NotFound($"Не найдено : продукт с id = {id}");
+                }
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Неизвестная ошибка в {nameof(GetProductById)}: " + ex.Message);
+            }
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(ProductModel), statusCode: StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> InsertProduct([FromBody] ProductModel product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"{ModelState}");
+            }
+
+            try
+            {
+                await repository.InsertAsync(product);
+                return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{ex.Message}");
+            }
+
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] ProductModel product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("ID не совпадает с ID в теле запроса! ");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"{ModelState}");
+            }
+            try
+            {
+                var existingProduct = await repository.GetByIdAsync(id);
+
+                if (existingProduct == null)
+                {
+                    return NotFound($"Продукт с таким Id [{id}] не найден!");
+                }
+                repository.Update(existingProduct);
+
+                return NoContent();
+            }
+
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,$"Неизвестная ошибка в методе {nameof(UpdateProduct)}: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> RemoveProduct(int id)
+        {
+            try
+            {
+                var product = await repository.GetByIdAsync(id);
+                if (product == null)
+                {
+                    return NotFound($"Продукта с ID = {id} не существует!");
+                }
+                repository.RemoveAsync(product);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Неизвестная ошибка: {ex.Message}");
+            }
+        }
+
+        [HttpGet("search")]
+        [ProducesResponseType(typeof(IEnumerable<ProductModel>), StatusCodes.Status200OK)]
+      
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> SearchProduct(string name)
+        {
+            try
+            {
+                var products =  await repository.FindAsync(p => p.ProductName != null && p.ProductName.Contains(name));
+
+                return Ok(products);
+            }catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"неизвестная ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ KursovayaBD/Controllers/ShopController.cs
@@ -0,0 +1,152 @@
+﻿using KursovayaBD.Models;
+using KursovayaBD.Repository.RepositoryImpl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace KursovayaBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        private readonly RepositoryShop repository;
+
+        public ShopController(RepositoryShop repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<ShopModel>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllShops()
+        {
+            try
+            {
+                var shops = await repository.GetAllAsync();
+                return Ok(shops);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ошибка во время получения данных о магазинах!");
+            }
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ShopModel), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetShopById(int id)
+        {
+            try
+            {
+                var shop = await repository.GetByIdAsync(id);
+                if (shop == null) { return NotFound($"Не найдено : Магазин с id = {id}"); }
+                return Ok(shop);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Неизвестная ошибка : {ex.Message}");
+            }
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShopModel), statusCode: StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> InsertShop([FromBody] ShopModel shop)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"{ModelState}");
+            }
+            try
+            {
+                await repository.InsertAsync(shop);
+                return CreatedAtAction(nameof(GetShopById), new { id = shop.Id }, shop);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ошибка добавления : {ex.Message}");
+            }
+
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> UpdateShop(int id, [FromBody] ShopModel shop)
+        {
+            if (id != shop.Id)
+            {
+                return BadRequest("Url ID совпадает с ID в теле запроса");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"{ModelState}");
+            }
+            try
+            {
+                var existingShop = await repository.GetByIdAsync(id);
+                if (existingShop == null)
+                {
+                    return NotFound($"Не существует магазина с таким Id [{id}]");
+                }
+                repository.Update(existingShop);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ошибка обновления : {ex.Message}");
+            }
+        }
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RemoveShop(int id)
+        {
+            try
+            {
+                var shop = await repository.GetByIdAsync(id);
+                if (shop == null)
+                {
+                    return BadRequest($"Не найдено магазин с Id = [{id}]");
+                }
+                repository.RemoveAsync(shop);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Неизвестная ошибка во время удаления shop - {ex.Message} ");
+
+
+            }
+        }
+        [HttpGet("search")]
+        [ProducesResponseType(typeof(IEnumerable<ShopModel>), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> SearchShops([FromQuery] string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("параметр поиска не установлен не установлен!");
+            }
+            try
+            {
+                var shops = await repository.FindAsync(s => s.ShopName != null && s.ShopName.Contains(name));
+
+                return Ok(shops);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Ошибка поиска : {ex.Message}");
+            }
+        }
+    }
+}
